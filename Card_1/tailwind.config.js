d99f77958d707e851f0281b1fa4fe73c0e2f3b05
Novaldi Sandi Ago <novaldisago@gmail.com>
2022-07-05 20:28:53 +0700

    finishing card html css ui

--- Card_1/tailwind.config.js
+++ Card_1/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'card': "url('../img/website_2.png')",
+      },
+      boxShadow: {
+        'card': "inset 0 1000px rgba(0,0,0,.7)",
+      }
+    },
   },
   plugins: [],
 }
